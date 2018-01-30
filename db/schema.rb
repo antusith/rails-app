ffa66b988d87094d7d916e103abdadd108572164
Anthony Tusithiphonexay <atusi@octo.com>
2018-01-30 14:57:08 +0100

    create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130132035) do
+ActiveRecord::Schema.define(version: 20180130135256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,5 +37,4 @@ ActiveRecord::Schema.define(version: 20180130132035) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "users"
 end
