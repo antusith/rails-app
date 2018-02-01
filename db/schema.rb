17142ca6a26055f6ccaf96b9d0427ef90c75d0e5
Anthony Tusithiphonexay <atusi@octo.com>
2018-02-01 19:08:50 +0100

    add secure password and attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131160550) do
+ActiveRecord::Schema.define(version: 20180201180200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20180131160550) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
